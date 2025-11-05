b9a1458a0e55b623924d42cced69f9a67456a878
FullMetal19 <manemamadouyaya@gmail.com>
2025-11-05 16:03:46 +0100

    new commit

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -13,7 +13,7 @@ export function Slide()
               <div className="d-flex align-items-center  mb-3" >
                 <span className="d-flex align-items-center justify-content-center bg-light px-4 py-2 rounded-2 shadow-sm"> 
                   {/* <i class="bi bi-house-door-fill fs-3 text-blue-clr"></i>  */}
-                  <img src={'../img/favicon.ico'} className="" alt="Logement extérieur" style={{ width: "100px" }} />
+                  <img src={'../favicon.ico'} className="" alt="Logement extérieur" style={{ width: "100px" }} />
                 </span>
               </div>
               <span className="h1 text-light"> ImoPro votre entreprise immobilière </span>
