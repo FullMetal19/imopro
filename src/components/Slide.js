67641c774d7b1fe06c8c23c369def5c7fa17c96b
FullMetal19 <manemamadouyaya@gmail.com>
2025-11-05 16:01:43 +0100

    new commit

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -11,7 +11,10 @@ export function Slide()
           <div className="row h-100 d-flex align-items-center ps-4" >
             <div className="col-md-10 d-flex flex-column p-4" >
               <div className="d-flex align-items-center  mb-3" >
-                <span className="d-flex align-items-center justify-content-center bg-light px-4 py-2 rounded-2 shadow-sm"> <i class="bi bi-house-door-fill fs-3 text-blue-clr"></i> </span>
+                <span className="d-flex align-items-center justify-content-center bg-light px-4 py-2 rounded-2 shadow-sm"> 
+                  {/* <i class="bi bi-house-door-fill fs-3 text-blue-clr"></i>  */}
+                  <img src={'../img/favicon.ico'} className="" alt="Logement extérieur" style={{ width: "100px" }} />
+                </span>
               </div>
               <span className="h1 text-light"> ImoPro votre entreprise immobilière </span>
               <span className="lead text-light" > Lorem Ipsum is simply dummy text of the and typesetting. Lorem Ipsum is simply dummy </span>
