82602265b014d8cf1ef7e93dfe60611ae5e556bc
FullMetal19 <manemamadouyaya@gmail.com>
2025-11-06 20:22:53 +0100

    new commit

--- src/views/admin/user/Modal.js
+++ src/views/admin/user/Modal.js
@@ -26,10 +26,10 @@ export function Modal({ method, message, uid, state })
     return (
 
         <div className="modal-container">
-            <div className="container pt-5">
-                <div className="row d-flex justify-content-center mt-5 px-2">
+            <div className="container">
+                <div className="row d-flex justify-content-center align-items-center vh-100 px-2">
                     <div className="col-lg-5 col-md-8 bg-three-clr p-4 d-flex flex-column">
-                        <div className="text-end"> <button className='btn btn-sm btn-white bold mb-2' onClick={ ()=>{ method ( false ) } } > X </button> </div>
+                        <div className="text-end"> <button className="btn-close btn-close-white position-absolute end-0 me-3" style={{ top: "10px" }} aria-label="Close" onClick={ ()=>{ method ( false ) } } ></button> </div>
                         <div className="d-flex border-top border-bottom pt-4 pb-2 mb-2">
                             <p className="text-muted"> {message} </p>
                         </div>
@@ -38,7 +38,7 @@ export function Modal({ method, message, uid, state })
                         }
                         {
                             ( status === 1 ) ? 
-                            (  <div className=""> <div className="alert alert-success border py-1 px-3 rounded-1 mb-4"> { ( state == 1 || state == 2 ) ? "Utilisateur bloqué avec succes" : "Utilisateur débloqué avec succes" }   </div> </div> ) :
+                            (  <div className=""> <div className="alert alert-primary border py-1 px-3 rounded-1 mb-4"> { ( state == 1 || state == 2 ) ? "Utilisateur bloqué avec succes" : "Utilisateur débloqué avec succes" }   </div> </div> ) :
                             ( status === -1 ) ?
                             (  <div className=""> <div className="alert alert-danger border py-1 px-3 rounded-1 mb-4"> Une erreur est survenue lors de la validation. </div> </div>) : null
                         }
